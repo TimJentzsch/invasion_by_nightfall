330e59de70aaa471e838b915de08128b7e5667a4
Tim Jentzsch <commits@timjen.net>
2024-05-17 11:57:48 +0200

    Spawn simple unit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
 fn main() {
-    App::new().add_systems(Startup, setup).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
 
-fn setup() {
-    println!("Hello world!")
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: Mesh2dHandle(meshes.add(Capsule2d::new(6.0, 15.0))),
+        material: materials.add(Color::WHITE),
+        ..default()
+    });
 }
