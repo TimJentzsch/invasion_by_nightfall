b7621ff9cafc7eb87ab864c5627d990ecefd3c27
Tim Jentzsch <commits@timjen.net>
2024-05-18 19:20:20 +0200

    Add unit cost text

--- src/ui.rs
+++ src/ui.rs
@@ -21,26 +21,70 @@ struct CoinText;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/fira_sans/FiraSans-Medium.ttf");
-    let style = TextStyle {
+    let header_style = TextStyle {
         font: font.clone(),
         font_size: 50.0,
         ..default()
     };
+    let footer_style = TextStyle {
+        font: font.clone(),
+        font_size: 30.0,
+        ..default()
+    };
 
     commands
-        .spawn(NodeBundle { ..default() })
+        .spawn(NodeBundle {
+            style: Style {
+                height: Val::Percent(100.),
+                width: Val::Percent(100.),
+                flex_direction: FlexDirection::Column,
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
+            },
+            ..default()
+        })
         .with_children(|child| {
-            child.spawn((
-                CoinText,
-                TextBundle::from_sections([
-                    TextSection::new("Coins: ", style.clone()),
-                    TextSection::new("0", style.clone()),
-                ]),
-            ));
+            // Header bar
+            child
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.),
+                        padding: UiRect::all(Val::Px(10.)),
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|child| {
+                    child.spawn((
+                        CoinText,
+                        TextBundle::from_sections([
+                            TextSection::new("0", header_style.clone()),
+                            TextSection::new(" G", header_style.clone()),
+                        ]),
+                    ));
+                });
+
+            // Bottom bar
+            child
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.),
+                        padding: UiRect::all(Val::Px(10.)),
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|child| {
+                    child.spawn((TextBundle::from_sections([
+                        TextSection::new("[Q] Farmer (", footer_style.clone()),
+                        TextSection::new("100", footer_style.clone()),
+                        TextSection::new(" G)", footer_style.clone()),
+                    ]),));
+                });
         });
 }
 
 fn update_coins(mut query: Query<&mut Text, With<CoinText>>, resources: Res<Resources>) {
     let mut text = query.single_mut();
-    text.sections[1].value = format!("{:.0}", resources.coins);
+    text.sections[0].value = format!("{:.0}", resources.coins);
 }
