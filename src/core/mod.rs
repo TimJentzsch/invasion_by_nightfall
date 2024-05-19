54f34ac95bb597a4666f8578a77cdda841da57a9
Tim Jentzsch <commits@timjen.net>
2024-05-19 17:08:31 +0200

    Implement basic attacks, damage and dying

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -27,8 +27,11 @@ impl Plugin for CorePlugin {
                         generate_waves.run_if(on_timer(Duration::from_secs(5))),
                     ),
                     spawn_unit,
-                    (unit_behavior, (move_units, attack_animation)).chain(),
-                    move_units,
+                    (
+                        unit_behavior,
+                        (move_units, (attack_animation, attack, die).chain()),
+                    )
+                        .chain(),
                 )
                     .chain()
                     .in_set(CoreSystemSet),
@@ -47,7 +50,12 @@ pub struct SpawnUnit {
 }
 
 #[derive(Debug, Event)]
-pub struct Attack;
+pub struct Attack {
+    is_foe: bool,
+    unit_stats: UnitStats,
+    transform: Transform,
+    direction: Vec3,
+}
 
 #[derive(Debug, Component)]
 pub struct Base;
@@ -75,25 +83,31 @@ impl UnitType {
     pub fn stats(&self) -> UnitStats {
         match *self {
             Self::Farmer => UnitStats {
+                health: 5.,
                 speed: 10.,
                 direction: Vec3::new(1., 0., 0.),
                 attack_range: 20.,
+                attack_damage: 2.,
             },
 
             Self::Shadow => UnitStats {
+                health: 10.,
                 speed: 10.,
                 direction: Vec3::new(-1., 0., 0.),
                 attack_range: 10.,
+                attack_damage: 2.,
             },
         }
     }
 }
 
-#[derive(Debug, Component)]
+#[derive(Debug, Component, Clone)]
 pub struct UnitStats {
+    health: f32,
     speed: f32,
-    direction: Vec3,
     attack_range: f32,
+    attack_damage: f32,
+    direction: Vec3,
 }
 
 #[derive(Debug, Component)]
@@ -223,10 +237,10 @@ fn move_units(
 fn attack_animation(
     mut commands: Commands,
     mut attack_event: EventWriter<Attack>,
-    mut unit_query: Query<(Entity, &mut Attacking), With<Unit>>,
+    mut unit_query: Query<(Entity, &mut Attacking, &Transform, &UnitStats, Has<Foe>), With<Unit>>,
     time: Res<Time>,
 ) {
-    for (entity, mut attacking) in unit_query.iter_mut() {
+    for (entity, mut attacking, transform, unit_stats, is_foe) in unit_query.iter_mut() {
         match &mut *attacking {
             Attacking::Start => {
                 // Start the foreswing anymation
@@ -235,7 +249,16 @@ fn attack_animation(
             Attacking::Foreswing(ref mut timer) => {
                 if timer.tick(time.delta()).finished() {
                     // After the foreswing is complete, execute the attack and start the backswing
-                    attack_event.send(Attack);
+                    attack_event.send(Attack {
+                        direction: if is_foe {
+                            Vec3::new(-1., 0., 0.)
+                        } else {
+                            Vec3::new(1., 0., 0.)
+                        },
+                        is_foe,
+                        transform: *transform,
+                        unit_stats: unit_stats.clone(),
+                    });
                     *attacking = Attacking::Backswing(Timer::from_seconds(0.5, TimerMode::Once));
                 }
             }
@@ -245,6 +268,60 @@ fn attack_animation(
                     commands.entity(entity).remove::<Attacking>();
                 }
             }
+        }
+    }
+}
+
+fn attack(
+    mut attack_event: EventReader<Attack>,
+    mut unit_query: Query<(&Transform, Has<Foe>, &mut UnitStats), With<Unit>>,
+) {
+    for Attack {
+        is_foe,
+        unit_stats,
+        transform,
+        direction,
+    } in attack_event.read()
+    {
+        let closest_unit = unit_query
+            .iter_mut()
+            .filter(|(other_transform, is_other_foe, _)| {
+                if is_foe == is_other_foe {
+                    return false;
+                }
+
+                let x = transform.translation.x;
+                let other_x = other_transform.translation.x;
+
+                let distance = other_x - x;
+
+                if distance.signum() != direction.x {
+                    // Only attack units in front of you
+                    return false;
+                }
+
+                // Only attack units within the attack range
+                distance.abs() <= unit_stats.attack_range
+            })
+            .min_by(|(a_transform, _, _), (b_transform, _, _)| {
+                let a_distance = transform.translation.distance(a_transform.translation);
+                let b_distance = transform.translation.distance(b_transform.translation);
+
+                a_distance
+                    .partial_cmp(&b_distance)
+                    .unwrap_or(std::cmp::Ordering::Less)
+            });
+
+        if let Some((_, _, mut stats)) = closest_unit {
+            stats.health -= unit_stats.attack_damage;
+        }
+    }
+}
+
+fn die(mut commands: Commands, unit_query: Query<(Entity, &UnitStats)>) {
+    for (unit, stats) in unit_query.iter() {
+        if stats.health <= 0. {
+            commands.entity(unit).despawn_recursive();
         }
     }
 }
