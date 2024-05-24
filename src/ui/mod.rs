c38f5633d81b69a7446d9d1fd51c9240abaac63a
Tim Jentzsch <commits@timjen.net>
2024-05-24 16:14:08 +0200

    Add simple post game UI

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,14 +1,15 @@
 use bevy::prelude::*;
 
-use self::in_game::InGameUiPlugin;
+use self::{in_game::InGameUiPlugin, post_game::PostGameUiPlugin};
 
 mod in_game;
+mod post_game;
 
 pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(InGameUiPlugin);
+        app.add_plugins((InGameUiPlugin, PostGameUiPlugin));
     }
 }
 
---
+++ src/ui/post_game.rs
@@ -0,0 +1,56 @@
+use bevy::prelude::*;
+
+use crate::core::{game_state::GameState, CoreSystemSet};
+
+use super::UiSystemSet;
+
+pub struct PostGameUiPlugin;
+
+impl Plugin for PostGameUiPlugin {
+    fn build(&self, app: &mut App) {
+        app.configure_sets(
+            OnEnter(GameState::PostGame),
+            UiSystemSet.after(CoreSystemSet),
+        )
+        .add_systems(OnEnter(GameState::PostGame), spawn.in_set(UiSystemSet))
+        .add_systems(OnExit(GameState::PostGame), despawn.in_set(UiSystemSet));
+    }
+}
+
+#[derive(Debug, Component)]
+struct PostGameUi;
+
+fn spawn(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/fira_sans/FiraSans-Medium.ttf");
+    let header_style = TextStyle {
+        font: font.clone(),
+        font_size: 150.0,
+        ..default()
+    };
+
+    commands
+        .spawn((
+            PostGameUi,
+            NodeBundle {
+                style: Style {
+                    height: Val::Percent(100.),
+                    width: Val::Percent(100.),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::SpaceBetween,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                ..default()
+            },
+        ))
+        .with_children(|child| {
+            // Header bar
+            child.spawn(TextBundle::from_section("Post Game", header_style.clone()));
+        });
+}
+
+fn despawn(mut commands: Commands, in_game_ui_query: Query<Entity, With<PostGameUi>>) {
+    for in_game_ui in in_game_ui_query.iter() {
+        commands.entity(in_game_ui).despawn_recursive();
+    }
+}
